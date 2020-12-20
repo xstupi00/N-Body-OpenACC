9fa04da231778d8af5b86d6ee955d8f220ec8df7
dd-20-28-245 <dd-20-28-245@cn203.bullx>
2020-12-20 01:14:41 +0100

    Add naive implementation of the COM

---
+++ step3/nbody.cpp
@@ -0,0 +1,146 @@
+/**
+ * @file      nbody.cpp
+ *
+ * @author    Jiri Jaros \n
+ *            Faculty of Information Technology \n
+ *            Brno University of Technology \n
+ *            jarosjir@fit.vutbr.cz
+ *
+ * @brief     PCG Assignment 2
+ *            N-Body simulation in ACC
+ *
+ * @version   2021
+ *
+ * @date      11 November  2020, 11:22 (created) \n
+ * @date      11 November  2020, 11:37 (revised) \n
+ *
+ */
+
+#include <math.h>
+#include <cfloat>
+#include "nbody.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                       Declare following structs / classes                                          //
+//                                  If necessary, add your own classes / routines                                     //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Compute gravitation and collision velocity, and subsequently update particle positions
+void calculate_velocity(
+  const Particles& p_in, Particles& p_out, const int N, const float dt
+) {
+
+#pragma acc parallel loop present(p_in, p_out) gang
+  for (unsigned int i = 0; i < N; i++)
+  {
+
+    /// Initialise of auxiliary accumulators of velocity
+    float tmp_vel_x = 0.0f;
+    float tmp_vel_y = 0.0f;
+    float tmp_vel_z = 0.0f;
+
+#pragma acc loop reduction(+:tmp_vel_x, tmp_vel_y, tmp_vel_z) worker vector
+    /// The iterations over all particles to compute the gravitation velocity to them
+    for (int j = 0; j < N; j++) {
+
+      /// Instruction Level Parallelism
+      float s = -G * dt * p_in.weights[j];
+      /// Computes the distance between the relevant particles
+      float r_x = p_in.pos_x[i] - p_in.pos_x[j];
+      float r_y = p_in.pos_y[i] - p_in.pos_y[j];
+      float r_z = p_in.pos_z[i] - p_in.pos_z[j];
+
+      /// Computes inverse distance between particles and their distances
+      float inv_dist = sqrtf(r_x * r_x + r_y * r_y + r_z * r_z);
+      /// Computes the gravitation velocity (Fg_dt_m2_r)
+      s /= (inv_dist * inv_dist * inv_dist + FLT_MIN);
+
+      /// The speed that a particle body receives due to the strength of the relevant particle
+      tmp_vel_x += (inv_dist > COLLISION_DISTANCE) ? r_x * s : 0.0f;
+      tmp_vel_y += (inv_dist > COLLISION_DISTANCE) ? r_y * s : 0.0f;
+      tmp_vel_z += (inv_dist > COLLISION_DISTANCE) ? r_z * s : 0.0f;
+
+      /// Checks whether the particles are in the sufficient near distance for collision
+      if (inv_dist > 0.0f && inv_dist < COLLISION_DISTANCE) {
+        /// Computes the temporary partial results to eliminate recalculation
+        float weight_diff = p_in.weights[i] - p_in.weights[j];
+        float weight_sum = p_in.weights[i] + p_in.weights[j];
+        float weight_j_x_2 = 2 * p_in.weights[j];
+
+        /// Computes the collision velocities between the relevant particles and accumulate the results
+        tmp_vel_x += inv_dist > 0.0f ?
+                     ((weight_diff * p_in.vel_x[i] + weight_j_x_2 * p_in.vel_x[j]) / weight_sum) - p_in.vel_x[i] : 0.0f;
+        tmp_vel_y += inv_dist > 0.0f ?
+                     ((weight_diff * p_in.vel_y[i] + weight_j_x_2 * p_in.vel_y[j]) / weight_sum) - p_in.vel_y[i] : 0.0f;
+        tmp_vel_z += inv_dist > 0.0f ?
+                     ((weight_diff * p_in.vel_z[i] + weight_j_x_2 * p_in.vel_z[j]) / weight_sum) - p_in.vel_z[i] : 0.0f;
+      }
+    }
+
+    /// Updates the velocity of particles with respect to the computed gravitation and collision velocity
+    p_out.vel_x[i] = p_in.vel_x[i] + tmp_vel_x;
+    p_out.vel_y[i] = p_in.vel_y[i] + tmp_vel_y;
+    p_out.vel_z[i] = p_in.vel_z[i] + tmp_vel_z;
+
+    /// Updates the positions of particles with respect to the updated velocity
+    p_out.pos_x[i] = p_in.pos_x[i] + (p_in.vel_x[i] + tmp_vel_x) * dt;
+    p_out.pos_y[i] = p_in.pos_y[i] + (p_in.vel_y[i] + tmp_vel_y) * dt;
+    p_out.pos_z[i] = p_in.pos_z[i] + (p_in.vel_z[i] + tmp_vel_z) * dt;
+  }
+
+}
+
+
+/// Compute center of gravity
+float4 centerOfMassGPU(const Particles& p, const int N)
+{
+  float sum_x = 0.0;  // Initialize overall sum of Positions X.
+  float sum_y = 0.0;  // Initialize overall sum of Positions Y.
+  float sum_z = 0.0;  // Initialize overall sum of Positions Z.
+  float sum_w = 0.0;  // Initialize overall sum of Weights.
+
+  /// Reduction across Multi-level Thread Parallelism in the Same Loop
+#pragma acc data copy(sum_x, sum_y, sum_z, sum_w)
+  {
+#pragma acc parallel loop present(p) reduction(+:sum_x, sum_y, sum_z, sum_w) gang worker vector
+    for (unsigned int i = 0; i < N; i++) {
+      sum_x += (p.pos_x[i] * p.weights[i]);   // Add the weighting position X of the current particle.
+      sum_y += (p.pos_y[i] * p.weights[i]);   // Add the weighting position Y of the current particle.
+      sum_z += (p.pos_z[i] * p.weights[i]);   // Add the weighting position Z of the current particle.
+      sum_w += p.weights[i];                  // Add the weights of the current particle.
+    }
+  }
+
+  /// Compute the final results of Center of Mass
+  /// x_com = (x1*w1 + x2*w2 + ... xn+wn) / (w1 + w2 + ... + wn)
+  return {sum_x / sum_w, sum_y / sum_w, sum_z / sum_w, sum_w};
+}/// end of centerOfMassGPU
+///---------------------------------------------------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Compute center of mass on CPU
+float4 centerOfMassCPU(MemDesc& memDesc)
+{
+  float4 com = {0 ,0, 0, 0};
+
+  for(int i = 0; i < memDesc.getDataSize(); i++)
+  {
+    // Calculate the vector on the line connecting points and most recent position of center-of-mass
+    const float dx = memDesc.getPosX(i) - com.x;
+    const float dy = memDesc.getPosY(i) - com.y;
+    const float dz = memDesc.getPosZ(i) - com.z;
+
+    // Calculate weight ratio only if at least one particle isn't massless
+    const float dw = ((memDesc.getWeight(i) + com.w) > 0.0f)
+                          ? ( memDesc.getWeight(i) / (memDesc.getWeight(i) + com.w)) : 0.0f;
+
+    // Update position and weight of the center-of-mass according to the weight ration and vector
+    com.x += dx * dw;
+    com.y += dy * dw;
+    com.z += dz * dw;
+    com.w += memDesc.getWeight(i);
+  }
+  return com;
+}// end of centerOfMassCPU
+//----------------------------------------------------------------------------------------------------------------------
---
+++ step3/nbody.h
@@ -0,0 +1,204 @@
+/**
+ * @file      nbody.h
+ *
+ * @author    Jiri Jaros \n
+ *            Faculty of Information Technology \n
+ *            Brno University of Technology \n
+ *            jarosjir@fit.vutbr.cz
+ *
+ * @brief     PCG Assignment 2
+ *            N-Body simulation in ACC
+ *
+ * @version   2021
+ *
+ * @date      11 November  2020, 11:22 (created) \n
+ * @date      11 November  2020, 11:37 (revised) \n
+ *
+ */
+
+#ifndef __NBODY_H__
+#define __NBODY_H__
+
+#include <cstdlib>
+#include <cstdio>
+#include  <cmath>
+#include "h5Helper.h"
+
+/// Gravity constant
+constexpr float G = 6.67384e-11f;
+
+/// Collision distance threshold
+constexpr float COLLISION_DISTANCE = 0.01f;
+
+/**
+ * @struct float4
+ * Structure that mimics CUDA float4
+ */
+struct float4
+{
+  float x;
+  float y;
+  float z;
+  float w;
+};
+
+/**
+ * @struct float3
+ * Structure that mimics CUDA float3
+ */
+struct float3
+{
+    float x;
+    float y;
+    float z;
+};
+
+/// Define sqrtf from CUDA libm library
+#pragma acc routine(sqrtf) seq
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                       Declare following structs / classes                                          //
+//                                  If necessary, add your own classes / routines                                     //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Structure with particle data
+ */
+struct Particles
+{
+  /// Fill the structure holding the particle/s data
+  /// It is recommended to implement constructor / destructor and copyToGPU and copyToCPU routines
+  size_t n;       // Number of particles.
+  float *pos_x;   // Positions in X
+  float *pos_y;   // Positions in Y
+  float *pos_z;   // Positions in Z
+  float *vel_x;   // Velocities in X
+  float *vel_y;   // Velocities in Y
+  float *vel_z;   // Velocities in Z
+  float *weights; // Weights
+
+  /// Default constructor is not allowed - the particles number must be known in advance
+  Particles() = delete;
+  /// Copy constructor is not allowed - performance loss due to data copies.
+  Particles(const Particles& stc) = delete;
+  /// Assignment operator is not allowed - performance loss due to data copies.
+  Particles& operator=(const Particles& stc) = delete;
+
+  /**
+  * Constructor.
+  * This routine construct the particles on both, the CPU and GPU.
+  * @param [in] n - Number of particles.
+  */
+  Particles(size_t n) : n(n)
+  {
+    /// Construct the particles on the CPU.
+    pos_x = new float[n];   // Allocates positions X.
+    pos_y = new float[n];   // Allocates positions Y.
+    pos_z = new float[n];   // Allocates positions Z.
+    vel_x = new float[n];   // Allocates velocities X.
+    vel_y = new float[n];   // Allocates velocities Y.
+    vel_z = new float[n];   // Allocates velocities Z.
+    weights = new float[n]; // Allocates weights.
+
+    /// Construct the particles on the GPU.
+    /// Copy the struct into device memory.
+    #pragma acc enter data copyin(this)
+    /// Allocate the dynamic members into device memory.
+    #pragma acc enter data create(pos_x[n])   // Allocates positions X.
+    #pragma acc enter data create(pos_y[n])   // Allocates positions Y.
+    #pragma acc enter data create(pos_z[n])   // Allocates positions Z.
+    #pragma acc enter data create(vel_x[n])   // Allocates velocities X.
+    #pragma acc enter data create(vel_y[n])   // Allocates velocities Y.
+    #pragma acc enter data create(vel_z[n])   // Allocates velocities Z.
+    #pragma acc enter data create(weights[n]) // Allocates weights.
+  }/// end of constructor
+  ///-----------------------------------------------------------------------------------------------------------
+
+  /**
+  * Destructor.
+  * This routine destruct the particles on both, the CPU and GPU.
+  */
+  ~Particles()
+  {
+    /// First deal with the deallocate of the dynamic members.
+    #pragma acc exit data delete(pos_x[n])   // Deallocate positions X.
+    #pragma acc exit data delete(pos_y[n])   // Deallocate positions Y.
+    #pragma acc exit data delete(pos_z[n])   // Deallocate positions Z.
+    #pragma acc exit data delete(vel_x[n])   // Deallocate velocities X.
+    #pragma acc exit data delete(vel_y[n])   // Deallocate velocities Y.
+    #pragma acc exit data delete(vel_z[n])   // Deallocate velocities Z.
+    #pragma acc exit data delete(weights[n]) // Deallocate weights.
+    /// Then deallocate the whole struct.
+    #pragma acc exit data delete(this)
+
+    /// Destruct the particles on the CPU.
+    delete [] pos_x;   // Deallocate positions X.
+    delete [] pos_y;   // Deallocate positions Y.
+    delete [] pos_z;   // Deallocate positions Z.
+    delete [] vel_x;   // Deallocate velocities X.
+    delete [] vel_y;   // Deallocate velocities Y.
+    delete [] vel_z;   // Deallocate velocities Z.
+    delete [] weights; // Deallocate weights.
+  } /// Destructor
+  ///------------------------------------------------------------------------------------------------------------------
+
+  /// Copy actual CPU particles data to GPU.
+  void copyToGPU()
+  {
+    #pragma acc update device(pos_x[n])   // Copy positions X: CPU => GPU.
+    #pragma acc update device(pos_y[n])   // Copy positions Y: CPU => GPU.
+    #pragma acc update device(pos_z[n])   // Copy positions Z: CPU => GPU.
+    #pragma acc update device(vel_x[n])   // Copy velocities X: CPU => GPU.
+    #pragma acc update device(vel_y[n])   // Copy velocities Y: CPU => GPU.
+    #pragma acc update device(vel_z[n])   // Copy velocities Z: CPU => GPU.
+    #pragma acc update device(weights[n]) // Copy weights: CPU => GPU.
+  }/// copyToGPU
+  ///------------------------------------------------------------------------------------------------------------------
+
+  /// Copy actual particles data from GPU to CPU.
+  void copyToCPU()
+  {
+    #pragma acc update host(pos_x[n])   // Copy positions X: GPU => CPU.
+    #pragma acc update host(pos_y[n])   // Copy positions Y: GPU => CPU.
+    #pragma acc update host(pos_z[n])   // Copy positions Z: GPU => CPU.
+    #pragma acc update host(vel_x[n])   // Copy velocities X: GPU => CPU.
+    #pragma acc update host(vel_y[n])   // Copy velocities Y: GPU => CPU.
+    #pragma acc update host(vel_z[n])   // Copy velocities Z: GPU => CPU.
+    #pragma acc update host(weights[n]) // Copy weights: GPU => CPU.
+  }/// copyToCPU
+  ///------------------------------------------------------------------------------------------------------------------
+
+};/// end of Particles
+///--------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Compute gravitation velocity
+ * @param [in]  p_in     - Particles to read (input particles)
+ * @param [out] p_out    - Particles to write (output particles)
+ * @param [in]  N        - Number of particles
+ * @param [in]  dt       - Time step size
+ */
+void calculate_velocity(
+  const Particles& p_in, Particles& p_out, const int N, const float dt
+);
+
+
+/**
+ * Compute center of gravity - implement in steps 3 and 4.
+ * @param [in] p - Particles
+ * @param [in] N - Number of particles
+ * @return Center of Mass [x, y, z] and total weight[w]
+ */
+float4 centerOfMassGPU(const Particles& p,
+                       const int        N);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Compute center of mass on CPU
+ * @param memDesc
+ * @return centre of gravity
+ */
+float4 centerOfMassCPU(MemDesc& memDesc);
+
+#endif /* __NBODY_H__ */
